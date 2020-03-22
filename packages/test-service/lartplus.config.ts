269b43c96477f3b439be17d40fb2ef78ecc01c54
wonderfulMan <1259243840@qq.com>
2020-03-22 18:10:01 +0800

    修改le rnalerna配置文件

--- packages/test-service/lartplus.config.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: hAo
- * @LastEditors  : hAo
- * @Date: 2020-03-18 15:38:16
- * @LastEditTime : 2020-03-19 11:21:47
- */
-module.exports = {
-    framework: 'react',
-}
--- packages/test-service/src/components/op/op/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const op: React.StatelessComponent = () => {
-    return (
-        <div>123</div>
-    );
-};
-
-export default op;
