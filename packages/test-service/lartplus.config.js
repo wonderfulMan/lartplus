94028c4d0ae1dbe698047418dcf25ce2bb8bb4b6
wonderfulMan <1259243840@qq.com>
2020-03-26 20:41:04 +0800

    更新babel等cli-service服务功能

---
+++ packages/test-service/lartplus.config.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: hAo
+ * @LastEditors  : hAo
+ * @Date: 2020-03-24 10:19:01
+ * @LastEditTime : 2020-03-24 21:59:11
+ */
+module.exports = {
+    framework: 'react',
+    typescript: true,
+    env: {
+        development: {
+            name: 'guohao'
+        },
+        builds: {
+            name: 'zhangsan'
+        }
+    }
+}
