2fbecbc82685ad618c89e09ba772e74c94b93e57
wonderfulMan <1259243840@qq.com>
2020-03-29 17:39:32 +0800

    获取依赖重构

--- packages/cli-generator/dist/lib/resolvedVersion.js
+++ packages/cli-generator/dist/lib/resolvedVersion.js
@@ -2,8 +2,8 @@
 exports.__esModule = true;
 /*
  * @Author: hAo
- * @LastEditors: hAo
+ * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime: 2020-03-28 17:30:24
+ * @LastEditTime : 2020-03-29 17:39:29
  */
-exports.lartplusCliVesion = "^0.0.16";
+exports.lartplusCliVesion = "^0.0.17";
