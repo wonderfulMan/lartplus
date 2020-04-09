808b270a923f7420c49000379555fde9b9c0536e
wonderfulMan <1259243840@qq.com>
2020-04-09 13:53:40 +0800

    修改依赖

--- packages/cli-generator/dist/lib/resolvedVersion.js
+++ packages/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-08 13:16:42
+ * @LastEditTime : 2020-04-09 13:53:31
  */
-exports.lartplusCliVesion = "^0.0.19";
+exports.lartplusCliVesion = "^0.0.20";
