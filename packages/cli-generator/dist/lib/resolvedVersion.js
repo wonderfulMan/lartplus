ad33f43f972b6c0acfc9d56d2ae4bfc885df4786
wonderfulMan <1259243840@qq.com>
2020-04-10 18:37:26 +0800

    走马灯

--- packages/cli-generator/dist/lib/resolvedVersion.js
+++ packages/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-10 17:45:57
+ * @LastEditTime : 2020-04-10 18:37:22
  */
-exports.lartplusCliVesion = "^0.0.28";
+exports.lartplusCliVesion = "^0.0.29";
