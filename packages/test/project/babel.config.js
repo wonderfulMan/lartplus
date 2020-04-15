01d383c663c24d8cdadc611666a8a589e0beb579
wonderfulMan <1259243840@qq.com>
2020-04-15 09:44:57 +0800

    test

--- packages/test/project/babel.config.js
+++ packages/test/project/babel.config.js
@@ -1,11 +1,4 @@
-/*
- * @Author: hAo
- * @LastEditors  : hAo
- * @Date: 2020-04-14 18:16:35
- * @LastEditTime : 2020-04-14 19:15:49
- */
-module.exports = (api) => {
-    api.cache(true);
+module.exports = (babel) => {
     return {
         presets: [["@babel/preset-env",{"corejs":3,"modules":false,"useBuiltIns":"usage"}],"@babel/preset-typescript"],
         plugins: ["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import","@babel/plugin-proposal-class-properties"]
