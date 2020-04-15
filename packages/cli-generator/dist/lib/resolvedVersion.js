9c2761b73cd9f7f234fc31dbc2080101b46383b1
wonderfulMan <1259243840@qq.com>
2020-04-15 17:50:20 +0800

    test vue

--- packages/cli-generator/dist/lib/resolvedVersion.js
+++ packages/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-15 16:15:20
+ * @LastEditTime : 2020-04-15 17:50:15
  */
-exports.lartplusCliVesion = "^0.0.40";
+exports.lartplusCliVesion = "^0.0.41";
