e962c34ceef01a4b0b1aff6b4125da7abc7b69a0
wonderfulMan <1259243840@qq.com>
2020-04-23 14:39:18 +0800

    update version

--- packages/cli-generator/dist/lib/resolvedVersion.js
+++ packages/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-23 10:12:07
+ * @LastEditTime : 2020-04-23 14:38:56
  */
-exports.lartplusCliVesion = "^0.0.55";
+exports.lartplusCliVesion = "^0.0.56";
--- packages/test/project/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (api) => {
-    api.cache(true);
-    return {
-        sourceType: "unambiguous",
-        presets: [["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import"],["@babel/preset-typescript",{"allExtensions":true,"isTSX":true}]],
-        plugins: [[["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import",["@babel/plugin-proposal-decorators",{"legacy":true}],["@babel/plugin-proposal-class-properties",{"loose":true}]]],[["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import",["@babel/plugin-proposal-decorators",{"legacy":true}],["@babel/plugin-proposal-class-properties",{"loose":true}]]],["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import"],["@babel/plugin-proposal-object-rest-spread","@babel/plugin-syntax-dynamic-import"],"@vue/transform-vue-jsx"]
-    }
-}
