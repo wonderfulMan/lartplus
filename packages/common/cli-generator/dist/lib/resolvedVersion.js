1f7db07f427b1f282be6e109fcf1850a27030ceb
wonderfulMan <1259243840@qq.com>
2020-04-28 11:20:35 +0800

    test

--- packages/common/cli-generator/dist/lib/resolvedVersion.js
+++ packages/common/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-28 11:13:26
+ * @LastEditTime : 2020-04-28 11:20:26
  */
-exports.lartplusCliVesion = "^0.0.61";
+exports.lartplusCliVesion = "^0.0.62";
