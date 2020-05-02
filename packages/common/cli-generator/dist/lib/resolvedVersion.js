15f0c487708229033bb675e2ceb52c2b2de70e97
wonderfulMan <1259243840@qq.com>
2020-05-02 15:21:25 +0800

    test vue generator

--- packages/common/cli-generator/dist/lib/resolvedVersion.js
+++ packages/common/cli-generator/dist/lib/resolvedVersion.js
@@ -4,6 +4,6 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-04-28 16:33:27
+ * @LastEditTime : 2020-05-02 15:20:30
  */
-exports.lartplusCliVesion = "^0.0.70";
+exports.lartplusCliVesion = "^0.0.72";
