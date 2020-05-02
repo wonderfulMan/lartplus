aab61df17ba55fe3c89fbf40a30022426497fd96
wonderfulMan <1259243840@qq.com>
2020-05-02 15:20:37 +0800

    test vue generator

--- packages/test/project/babel.config.js
+++ packages/test/project/babel.config.js
@@ -1,33 +1,8 @@
-/*
- * @Author: hAo
- * @LastEditors: hAo
- * @Date: 2020-05-02 14:25:31
- * @LastEditTime: 2020-05-02 14:28:46
- */
 module.exports = (api) => {
     api.cache(true);
     return {
         sourceType: "unambiguous",
-        presets: [
-            ["@babel/preset-env", {
-                "corejs": 3,
-                "modules": false,
-                "useBuiltIns": "usage"
-            }],
-            ["@babel/preset-typescript", {
-                "allExtensions": true,
-                "isTSX": true
-            }]
-        ],
-        plugins: [
-            ["@babel/plugin-proposal-object-rest-spread"],
-            ["@babel/plugin-syntax-dynamic-import"],
-            ["@babel/plugin-proposal-decorators", {
-                "legacy": true
-            }],
-            ["@babel/plugin-proposal-class-properties", {
-                "loose": true
-            }], "@vue/transform-vue-jsx"
-        ]
+        presets: [["@babel/preset-env",{"corejs":3,"modules":false,"useBuiltIns":"usage"}],["@babel/preset-typescript",{"allExtensions":true,"isTSX":true}]],
+        plugins: [["@babel/plugin-proposal-object-rest-spread"],["@babel/plugin-syntax-dynamic-import"],["@babel/plugin-proposal-decorators",{"legacy":true}],["@babel/plugin-proposal-class-properties",{"loose":true}],"@vue/transform-vue-jsx"]
     }
 }
