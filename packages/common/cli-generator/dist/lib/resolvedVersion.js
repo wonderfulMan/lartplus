decce61e5c694fc82dc979888b17a6efa899d091
wonderfulMan <1259243840@qq.com>
2020-05-07 19:35:37 +0800

    test

--- packages/common/cli-generator/dist/lib/resolvedVersion.js
+++ packages/common/cli-generator/dist/lib/resolvedVersion.js
@@ -4,7 +4,7 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-05-07 15:07:28
+ * @LastEditTime : 2020-05-07 19:35:07
  */
 function getLartplusCliVesion() {
     var cliPackage = require('../../package.json');
