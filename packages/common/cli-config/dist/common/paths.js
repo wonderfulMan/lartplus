7d766c1f0fda73995bebd48d09bafced863c2d14
wonderfulMan <1259243840@qq.com>
2020-05-14 14:28:46 +0800

    test

--- packages/common/cli-config/dist/common/paths.js
+++ packages/common/cli-config/dist/common/paths.js
@@ -4,14 +4,20 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-05-07 11:09:25
- * @LastEditTime : 2020-05-07 13:16:17
+ * @LastEditTime : 2020-05-14 14:18:21
  */
 exports.PATHS = {
+    // 目标项目
     getLartPlusModulePath: function (target) { return target + "/node_modules/@lartplus"; },
     getEslintRcPath: function (target) { return target + "/.eslintrc.js"; },
     getPrettierRcPath: function (target) { return target + "/.prettierrc.js"; },
     getLartPlusFilePath: function (target) { return target + "/lartplus.config.js"; },
     getBableConfigFilePath: function (target) { return target + "/babel.config.js"; },
     getProjectPackagePath: function (target) { return target + "/package.json"; },
-    getProjectTsconfigFilePath: function (target) { return target + "/tsconfig.json"; }
+    getProjectTsconfigFilePath: function (target) { return target + "/tsconfig.json"; },
+    getPostcssConfigFilePath: function (target) { return target + "/postcss.config.js"; },
+    // 模版
+    getLartPlusTemplatePath: function (target) { return target + "/template/lartplus.config.tpl"; },
+    getLartPlusTsconfigTemplatePath: function (target) { return target + "/template/tsconfig.config.tpl"; },
+    getLartPlusBabelTemplatePath: function (target) { return target + "/template/babel.config.tpl"; }
 };
