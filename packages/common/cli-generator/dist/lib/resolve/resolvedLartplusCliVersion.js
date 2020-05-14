3b54a20758eacee4df78cc82eef7f25cac1a15d4
wonderfulMan <1259243840@qq.com>
2020-05-14 09:08:50 +0800

    test

--- packages/common/cli-generator/dist/lib/resolve/resolvedLartplusCliVersion.js
+++ packages/common/cli-generator/dist/lib/resolve/resolvedLartplusCliVersion.js
@@ -4,10 +4,10 @@ exports.__esModule = true;
  * @Author: hAo
  * @LastEditors  : hAo
  * @Date: 2020-03-28 17:30:13
- * @LastEditTime : 2020-05-13 18:01:51
+ * @LastEditTime : 2020-05-14 09:08:42
  */
 function resolvedLartplusCliVersion() {
-    var cliPackage = require('../../package.json');
+    var cliPackage = require('../../../package.json');
     var cliVersion = cliPackage.version;
     return cliVersion;
 }
