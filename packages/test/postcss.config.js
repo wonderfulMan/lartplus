dbe070039ae6b48a5dfaa7182195e56eee6fcd53
wonderfulMan <1259243840@qq.com>
2020-06-29 10:39:18 +0800

    test

--- packages/test/postcss.config.js
+++ packages/test/postcss.config.js
@@ -1,1 +1,19 @@
-module.exports = [object Object]
+module.exports = {
+  "plugins": [
+    {
+      "postcss-px-to-viewport": {
+        "unitToConvert": "px",
+        "viewportWidth": 750,
+        "viewportHeight": 1334,
+        "unitPrecision": 3,
+        "viewportUnit": "vw",
+        "selectorBlackList": [
+          ".ignore",
+          ".hairlines"
+        ],
+        "minPixelValue": 1,
+        "mediaQuery": false
+      }
+    }
+  ]
+}
